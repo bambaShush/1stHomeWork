6521c4f5f03c961db03c2f680cf73ba5dad7bb93
bambaShush <dan_ko@outlook.com>
2018-04-13 17:39:59 +0300

    commit 1

--- app/src/main/java/com/example/user/a1sthomework/MainActivity.java
+++ app/src/main/java/com/example/user/a1sthomework/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.user.a1sthomework;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    String[] member_names;
+    TypedArray profile_pics;
+    String[] statues;
+    String[] contactType;
+
+
+
+    List<RowItem> rowItems;
+    ListView mylistview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        member_names = getBaseContext().getResources().getStringArray(R.array.Member_names);
+        profile_pics = getBaseContext().getResources().obtainTypedArray(R.array.profile_pics);
+        contactType = getBaseContext().getResources().getStringArray(R.array.contactType);
+        statues = getBaseContext().getResources().getStringArray(R.array.statues);
+
+        rowItems = new ArrayList<RowItem>();
+        profile_pics = getResources().obtainTypedArray(R.array.profile_pics);
+
+        statues = getResources().getStringArray(R.array.statues);
+
+        contactType = getResources().getStringArray(R.array.contactType);
+
+        for (int i = 0; i < member_names.length; i++) {
+            RowItem item = new RowItem(member_names[i],
+                    profile_pics.getResourceId(i, -1), statues[i],
+                    contactType[i]);
+            rowItems.add(item);
+        }
+
+        mylistview = (ListView) findViewById(R.id.list);
+        CustomAdapter adapter = new CustomAdapter(this, rowItems);
+        mylistview.setAdapter(adapter);
+
+        mylistview.setOnItemClickListener(this);
+
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+
+        String member_name = rowItems.get(position).getMember_name();
+        Toast.makeText(getApplicationContext(), "" + member_name,
+                Toast.LENGTH_SHORT).show();
+    }
+
 }
